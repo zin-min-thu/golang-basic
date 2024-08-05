1fdf4102113108d3ecb94d52b5719348a785e2e9
tommy <tommy@visibleone.com>
2024-08-05 14:48:23 +0630

    learn post request

--- 22webrequest/main.go
+++ 22webrequest/main.go
@@ -10,7 +10,9 @@ import (
 func main() {
 	fmt.Println("Welcome GET request in golang")
 
-	PerformGetRequest()
+	// PerformGetRequest()
+
+	PerformPostJsonRequest()
 }
 
 func PerformGetRequest() {
@@ -36,4 +38,28 @@ func PerformGetRequest() {
 	fmt.Println(responseString.String())
 
 	// fmt.Println(string(content))
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:8001/api/post"
+
+	requestBody := strings.NewReader(`
+		{
+			"coursename" : "Let's go with golang",
+			"price" : 12,
+			"platform" : "google.com"
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
 }
